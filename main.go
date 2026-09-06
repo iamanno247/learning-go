d888a7c7d66efaaac9f3d0c66785565f6553f0f6
ANNO <iamanno247@gmail.com>
2026-09-06 21:47:25 +0600

    Sum-Two-Numbers

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    // Print the greeting below.
-	fmt.Println("Hello, Go!")
-}
+    var a, b int
+    fmt.Scan(&a)
+    fmt.Scan(&b)
+    // Print a + b.
+	sum := float64(a) + float64(b)
+	fmt.Print(sum)
+ }
+
