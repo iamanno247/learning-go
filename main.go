4ddfde23e721682bae389bdd84e33e05337c4123
ANNO <iamanno247@gmail.com>
2026-09-07 13:45:00 +0600

    rectangle-area

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    var a, b int
-    fmt.Scan(&a)
-    fmt.Scan(&b)
-    // Print a + b.
-	sum := float64(a) + float64(b)
-	fmt.Print(sum)
- }
-
+	var w, h int
+	fmt.Scan(&w, &h)
+	area := w * h 
+	fmt.Print(area)
+}
