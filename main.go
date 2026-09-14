08d586e2cff1bd5dba4cb8fe69cb32ce72eb94b6
ANNO <iamanno247@gmail.com>
2026-09-14 17:42:58 +0600

    slices

--- main.go
+++ main.go
@@ -9,13 +9,21 @@ import (
 )
 
 func main()  {
-	r := bufio.NewReader(os.Stdin)
-	numStr, _ := r.ReadString('\n')
-	numStr = strings.TrimRight(numStr, "\r\n")
-	num, err := strconv.Atoi(numStr)
-	if err != nil {
-		fmt.Println("bad")
-	} else {
-		fmt.Printf("ok %d", num)
+	r := bufio.NewReader(os.Stdin) // taking input
+	values, _ := r.ReadString('\n')  // reading input to /n
+	// trims space and separates the nums with space inbetween
+	parts := strings.Fields(strings.TrimSpace(values))
+	nums := make([]int, 0, len(parts)) // making an empty slice(string)
+	// for loop to convert the input from strings to int
+	for _, p := range parts{
+		n, _ := strconv.Atoi(p)
+		nums = append(nums, n)
 	}
+  maximum := nums[0] // seeds the first num from nums this fixs neg nums
+	for _, n := range nums{
+		if n > maximum {
+			maximum = n
+		}
+	}
+	fmt.Println(maximum)
 }
