167afae5969849144c85ddd91ec59ce853e3f7bb
ANNO <iamanno247@gmail.com>
2026-09-14 22:04:17 +0600

    maps

--- main.go
+++ main.go
@@ -3,27 +3,18 @@ package main
 import (
 	"fmt"
 	"strings"
-	"strconv"
 	"os"
 	"bufio"
 )
 
+
 func main()  {
-	r := bufio.NewReader(os.Stdin) // taking input
-	values, _ := r.ReadString('\n')  // reading input to /n
-	// trims space and separates the nums with space inbetween
-	parts := strings.Fields(strings.TrimSpace(values))
-	nums := make([]int, 0, len(parts)) // making an empty slice(string)
-	// for loop to convert the input from strings to int
-	for _, p := range parts{
-		n, _ := strconv.Atoi(p)
-		nums = append(nums, n)
+	r := bufio.NewReader(os.Stdin)
+	line, _ := r.ReadString('\n')
+	words := strings.Fields(strings.TrimSpace(line))
+	mappedWords := make(map[string]bool)
+	for _, words := range words{
+		mappedWords[words] = true
 	}
-  maximum := nums[0] // seeds the first num from nums this fixs neg nums
-	for _, n := range nums{
-		if n > maximum {
-			maximum = n
-		}
-	}
-	fmt.Println(maximum)
+	fmt.Println(len(mappedWords))
 }
