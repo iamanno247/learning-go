65f5b53a907f5366cc798bf1ecb39e642d588a55
ANNO <iamanno247@gmail.com>
2026-09-17 20:26:49 +0600

    pointersInGO

--- main.go
+++ main.go
@@ -1,22 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Point struct {
-	X, Y int
+func increment(n *int) {
+    // dereference and increment
+		*n++
 }
 
-func main()  {
-	var x1, y1, x2, y2 int
-	fmt.Scan(&x1)
-  fmt.Scan(&y1)
-	fmt.Scan(&x2)
-	fmt.Scan(&y2)
-	p1 := Point{X: x1, Y: y1}
-	p2 := Point{X: x2, Y: y2}
-	distance := (p2.X - p1.X) * (p2.X - p1.X) + (p2.Y - p1.Y) * (p2.Y - p1.Y)
-
-	fmt.Printf("%d", distance)
+func main() {
+    var x int
+    fmt.Scan(&x)
+    increment(&x)
+    fmt.Println(x)
 }
